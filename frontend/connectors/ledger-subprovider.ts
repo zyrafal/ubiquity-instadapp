ec20fbe28797dc91da535e517016ae9527d2f5d4
zapaz.eth <alain@kredeum.com>
2022-01-24 16:22:00 +0100

    try pnpm on frontend

--- frontend/connectors/ledger-subprovider.ts
+++ frontend/connectors/ledger-subprovider.ts
@@ -1,8 +1,8 @@
-import AppEth from '@ledgerhq/hw-app-eth';
-import type Transport from '@ledgerhq/hw-transport';
-import HookedWalletSubprovider from 'web3-provider-engine/subproviders/hooked-wallet';
-import stripHexPrefix from 'strip-hex-prefix';
-import { Transaction as EthereumTx } from 'ethereumjs-tx';
+import AppEth from "@ledgerhq/hw-app-eth";
+import type Transport from "@ledgerhq/hw-transport";
+import HookedWalletSubprovider from "web3-provider-engine/subproviders/hooked-wallet";
+import stripHexPrefix from "strip-hex-prefix";
+import { Transaction as EthereumTx } from "ethereumjs-tx";
 
 function makeError(msg: string, id: string) {
   const err: any = new Error(msg);
@@ -40,7 +40,7 @@ export default async function createLedgerSubprovider(
   getTransport: () => Promise<Transport>,
   options?: SubproviderOptions
 ) {
-  if (options && 'path' in options) {
+  if (options && "path" in options) {
     throw new Error(
       "@ledgerhq/web3-subprovider: path options was replaced by paths. example: paths: [\"44'/60'/x'/0/0\"]"
     );
@@ -51,7 +51,7 @@ export default async function createLedgerSubprovider(
   };
 
   if (!paths.length) {
-    throw new Error('paths must not be empty');
+    throw new Error("paths must not be empty");
   }
 
   const addressToPathMap: any = {};
@@ -63,7 +63,7 @@ export default async function createLedgerSubprovider(
     for (let i = accountsOffset; i < accountsOffset + accountsLength; i++) {
       const x = Math.floor(i / paths.length);
       const pathIndex = i - paths.length * x;
-      const path = paths[pathIndex].replace('x', String(x));
+      const path = paths[pathIndex].replace("x", String(x));
       const address = await eth.getAddress(path, askConfirm, false);
       addresses[path] = address.address;
       addressToPathMap[address.address.toLowerCase()] = path;
@@ -75,8 +75,11 @@ export default async function createLedgerSubprovider(
     const path = addressToPathMap[msgData.from.toLowerCase()];
     if (!path) throw new Error("address unknown '" + msgData.from + "'");
     const eth = new AppEth(transport);
-    const result = await eth.signPersonalMessage(path, stripHexPrefix(msgData.data));
-    const v = parseInt(result.v, 10) - 27;
+    const result = await eth.signPersonalMessage(
+      path,
+      stripHexPrefix(msgData.data)
+    );
+    const v = parseInt(String(result.v), 10) - 27;
     let vHex = v.toString(16);
     if (vHex.length < 2) {
       vHex = `0${v}`;
@@ -96,24 +99,30 @@ export default async function createLedgerSubprovider(
     tx.raw[8] = Buffer.from([]); // s
 
     // Pass hex-rlp to ledger for signing
-    const result = await eth.signTransaction(path, tx.serialize().toString('hex'));
+    const result = await eth.signTransaction(
+      path,
+      tx.serialize().toString("hex")
+    );
 
     // Store signature in transaction
-    tx.v = Buffer.from(result.v, 'hex');
-    tx.r = Buffer.from(result.r, 'hex');
-    tx.s = Buffer.from(result.s, 'hex');
+    tx.v = Buffer.from(result.v, "hex");
+    tx.r = Buffer.from(result.r, "hex");
+    tx.s = Buffer.from(result.s, "hex");
 
     // EIP155: v should be chain_id * 2 + {35, 36}
     const signedChainId = Math.floor((tx.v[0] - 35) / 2);
     const validChainId = networkId & 0xff; // FIXME this is to fixed a current workaround that app don't support > 0xff
     if (signedChainId !== validChainId) {
       throw makeError(
-        'Invalid networkId signature returned. Expected: ' + networkId + ', Got: ' + signedChainId,
-        'InvalidNetworkId'
+        "Invalid networkId signature returned. Expected: " +
+          networkId +
+          ", Got: " +
+          signedChainId,
+        "InvalidNetworkId"
       );
     }
 
-    return `0x${tx.serialize().toString('hex')}`;
+    return `0x${tx.serialize().toString("hex")}`;
   }
 
   const subprovider = new HookedWalletSubprovider({
@@ -122,7 +131,10 @@ export default async function createLedgerSubprovider(
         .then((res) => callback(null, Object.values(res)))
         .catch((err) => callback(err, null));
     },
-    signPersonalMessage: (txData: any, callback: (err: any, res: any) => void) => {
+    signPersonalMessage: (
+      txData: any,
+      callback: (err: any, res: any) => void
+    ) => {
       signPersonalMessage(txData)
         .then((res) => callback(null, res))
         .catch((err) => callback(err, null));
